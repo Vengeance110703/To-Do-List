5b28bdbe0f2cd336fb18345f817095e07b9dcfba
Vengeance110703 <yashsheth66@gmail.com>
2023-03-22 12:56:20 +0530

    Add new task

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
+var listItems = ["Buy Food","Cook Food","Eat Food"]
+
 app.listen(3000, () => {
     console.log("Server is listenig on port 3000")
 })
@@ -13,8 +15,17 @@ app.get("/", (req, res) => {
 
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var d = new Date();
-    console.log(d);
     var day = weekday[d.getDay()]
 
-    res.render("list", { day: day })
+    res.render("list", { 
+        day: day,
+        newItem: listItems, 
+    })
+})
+
+app.post("/", (req, res) => {
+    
+    listItems.push(req.body.newItem);
+    res.redirect("/")
+
 })
